2deb1745e52ab7a2ba8a0761713b5a52d8275fc1
funbiscuit <kokurinsv@gmail.com>
2022-10-11 19:17:12 +0300

    Decouple input handling between app and terminal

--- cli/src/app.rs
+++ cli/src/app.rs
@@ -3,6 +3,7 @@ use spacedisplay_lib::{
 };
 
 use crate::file_list::FileListState;
+use crate::term::{InputHandler, InputProvider};
 
 #[derive(Clone, Copy, Debug, Eq, PartialEq)]
 pub enum Screen {
@@ -38,72 +39,12 @@ impl App {
         }
     }
 
-    pub fn on_backspace(&mut self) {
-        if !self.current_path.is_root() && self.screen == Screen::Files {
-            let name = self.current_path.get_name().to_string();
-            self.current_path.go_up();
-            self.update_snapshot();
-            self.select_entry(&name);
-        }
-    }
-
-    pub fn on_down(&mut self) {
-        if self.screen == Screen::Files {
-            self.file_list_state
-                .select(self.file_list_state.selected() + 1);
-        }
-    }
-
-    pub fn on_enter(&mut self) {
-        if self.screen == Screen::Files {
-            if let Some(snapshot) = self.snapshot.as_ref() {
-                let files: Vec<_> = snapshot.get_root().iter().collect();
-                if let Some(entry) = files.get(self.file_list_state.selected()) {
-                    if entry.is_dir() {
-                        self.current_path.join(entry.get_name().to_string());
-                        self.file_list_state.select(0);
-                    }
-                }
-            }
-        }
-    }
-
-    pub fn on_esc(&mut self) {
-        self.on_backspace();
-    }
-
-    pub fn on_fn(&mut self, n: u8) {
-        if n == 1 {
-            self.screen = Screen::Help;
-        }
-    }
-
-    pub fn on_key(&mut self, c: char) {
-        match c {
-            'h' => self.screen = Screen::Help,
-            'f' => self.screen = Screen::Files,
-            'q' => self.should_quit = true,
-            _ => {}
-        }
-    }
-
-    pub fn on_left(&mut self) {
-        self.on_backspace();
-    }
-
-    pub fn on_right(&mut self) {
-        self.on_enter();
+    pub fn check_input<H: InputProvider>(&mut self, provider: &H) {
+        let _ = provider.provide(self);
     }
 
     pub fn on_tick(&mut self) {
         self.update_snapshot();
-    }
-
-    pub fn on_up(&mut self) {
-        let selected = self.file_list_state.selected();
-        if self.screen == Screen::Files && selected > 0 {
-            self.file_list_state.select(selected - 1);
-        }
     }
 
     pub fn selected_tab(&self) -> usize {
@@ -166,6 +107,72 @@ impl App {
 
         if let Some(name) = selected {
             self.select_entry(&name);
+        }
+    }
+}
+
+impl InputHandler for App {
+    fn on_backspace(&mut self) {
+        if !self.current_path.is_root() && self.screen == Screen::Files {
+            let name = self.current_path.get_name().to_string();
+            self.current_path.go_up();
+            self.update_snapshot();
+            self.select_entry(&name);
+        }
+    }
+
+    fn on_down(&mut self) {
+        if self.screen == Screen::Files {
+            self.file_list_state
+                .select(self.file_list_state.selected() + 1);
+        }
+    }
+
+    fn on_enter(&mut self) {
+        if self.screen == Screen::Files {
+            if let Some(snapshot) = self.snapshot.as_ref() {
+                let files: Vec<_> = snapshot.get_root().iter().collect();
+                if let Some(entry) = files.get(self.file_list_state.selected()) {
+                    if entry.is_dir() {
+                        self.current_path.join(entry.get_name().to_string());
+                        self.file_list_state.select(0);
+                    }
+                }
+            }
+        }
+    }
+
+    fn on_esc(&mut self) {
+        self.on_backspace();
+    }
+
+    fn on_fn(&mut self, n: u8) {
+        if n == 1 {
+            self.screen = Screen::Help;
+        }
+    }
+
+    fn on_key(&mut self, c: char) {
+        match c {
+            'h' => self.screen = Screen::Help,
+            'f' => self.screen = Screen::Files,
+            'q' => self.should_quit = true,
+            _ => {}
+        }
+    }
+
+    fn on_left(&mut self) {
+        self.on_backspace();
+    }
+
+    fn on_right(&mut self) {
+        self.on_enter();
+    }
+
+    fn on_up(&mut self) {
+        let selected = self.file_list_state.selected();
+        if self.screen == Screen::Files && selected > 0 {
+            self.file_list_state.select(selected - 1);
         }
     }
 }
--- cli/src/term.rs
+++ cli/src/term.rs
@@ -14,6 +14,80 @@ use spacedisplay_lib::Scanner;
 use crate::app::App;
 use crate::{ui, Args};
 
+pub trait InputHandler {
+    fn on_backspace(&mut self) {}
+    fn on_down(&mut self) {}
+    fn on_enter(&mut self) {}
+    fn on_esc(&mut self) {}
+    fn on_fn(&mut self, _n: u8) {}
+    fn on_key(&mut self, _c: char) {}
+    fn on_left(&mut self) {}
+    fn on_right(&mut self) {}
+    fn on_up(&mut self) {}
+}
+
+pub trait InputProvider {
+    fn provide<T: InputHandler>(&self, t: &mut T) -> Result<()>;
+}
+
+struct AppRunner<'a, B: Backend> {
+    terminal: &'a mut Terminal<B>,
+    tick_rate: Duration,
+    last_tick: Instant,
+}
+
+impl<'a, B: Backend> AppRunner<'a, B> {
+    fn new(terminal: &'a mut Terminal<B>, tick_rate: Duration) -> Self {
+        Self {
+            terminal,
+            tick_rate,
+            last_tick: Instant::now(),
+        }
+    }
+
+    fn run(mut self, mut app: App) -> Result<()> {
+        loop {
+            self.terminal.draw(|f| ui::draw(f, &mut app))?;
+
+            app.check_input(&self);
+            if self.last_tick.elapsed() >= self.tick_rate {
+                app.on_tick();
+                self.last_tick = Instant::now();
+            }
+            if app.should_quit {
+                return Ok(());
+            }
+        }
+    }
+}
+
+impl<'a, B: Backend> InputProvider for AppRunner<'a, B> {
+    fn provide<T: InputHandler>(&self, handler: &mut T) -> Result<()> {
+        let timeout = self
+            .tick_rate
+            .checked_sub(self.last_tick.elapsed())
+            .unwrap_or_else(|| Duration::from_secs(0));
+        if event::poll(timeout)? {
+            if let CEvent::Key(key) = event::read()? {
+                match key.code {
+                    KeyCode::Char(c) => handler.on_key(c),
+                    KeyCode::Up => handler.on_up(),
+                    KeyCode::Down => handler.on_down(),
+                    KeyCode::Left => handler.on_left(),
+                    KeyCode::Right => handler.on_right(),
+                    KeyCode::Enter => handler.on_enter(),
+                    KeyCode::Esc => handler.on_esc(),
+                    KeyCode::Backspace => handler.on_esc(),
+                    KeyCode::F(n) => handler.on_fn(n),
+                    _ => {}
+                }
+            }
+        }
+
+        Ok(())
+    }
+}
+
 pub fn run(args: Args) -> Result<()> {
     let original_hook = std::panic::take_hook();
     std::panic::set_hook(Box::new(move |panic| {
@@ -22,9 +96,9 @@ pub fn run(args: Args) -> Result<()> {
     }));
 
     let mut terminal = init_terminal()?;
-
+    let runner = AppRunner::new(&mut terminal, args.tick_rate);
     let app = App::new(Scanner::new(args.path));
-    let res = run_app(&mut terminal, app, args.tick_rate);
+    let res = runner.run(app);
 
     reset_terminal()?;
 
@@ -47,42 +121,4 @@ fn reset_terminal() -> Result<()> {
     execute!(std::io::stdout(), LeaveAlternateScreen, DisableMouseCapture)?;
 
     Ok(())
-}
-
-fn run_app<B: Backend>(
-    terminal: &mut Terminal<B>,
-    mut app: App,
-    tick_rate: Duration,
-) -> Result<()> {
-    let mut last_tick = Instant::now();
-    loop {
-        terminal.draw(|f| ui::draw(f, &mut app))?;
-
-        let timeout = tick_rate
-            .checked_sub(last_tick.elapsed())
-            .unwrap_or_else(|| Duration::from_secs(0));
-        if event::poll(timeout)? {
-            if let CEvent::Key(key) = event::read()? {
-                match key.code {
-                    KeyCode::Char(c) => app.on_key(c),
-                    KeyCode::Up => app.on_up(),
-                    KeyCode::Down => app.on_down(),
-                    KeyCode::Left => app.on_left(),
-                    KeyCode::Right => app.on_right(),
-                    KeyCode::Enter => app.on_enter(),
-                    KeyCode::Esc => app.on_esc(),
-                    KeyCode::Backspace => app.on_esc(),
-                    KeyCode::F(n) => app.on_fn(n),
-                    _ => {}
-                }
-            }
-        }
-        if last_tick.elapsed() >= tick_rate {
-            app.on_tick();
-            last_tick = Instant::now();
-        }
-        if app.should_quit {
-            return Ok(());
-        }
-    }
 }
