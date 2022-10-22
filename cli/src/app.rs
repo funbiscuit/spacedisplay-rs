82cbeeff45d326eec92234e5f3d05b6841d92b9e
funbiscuit <kokurinsv@gmail.com>
2022-10-22 17:49:40 +0300

    Remember selected entries

--- cli/src/app.rs
+++ cli/src/app.rs
@@ -1,8 +1,7 @@
-use byte_unit::Byte;
 use derivative::Derivative;
 
 use spacedisplay_lib::{
-    EntryPath, EntrySnapshot, ScanStats, Scanner, SnapshotConfig, TreeSnapshot,
+    EntryPath, EntrySnapshot, EntrySnapshotRef, ScanStats, Scanner, SnapshotConfig, TreeSnapshot,
 };
 
 use crate::dialog::{DeleteDialog, Dialog, NewScanDialog, ScanStatsDialog};
@@ -20,6 +19,7 @@ pub struct FilesApp {
     pub scanner: Scanner,
     pub file_list_state: FileListState,
     pub current_path: EntryPath,
+    pub path_history: Vec<String>,
     pub snapshot: Option<TreeSnapshot<EntrySnapshot>>,
     pub stats: ScanStats,
 }
@@ -34,27 +34,24 @@ impl FilesApp {
             scanner,
             file_list_state,
             current_path,
+            path_history: vec![],
             snapshot: None,
             stats,
         }
     }
 
-    pub fn get_selected(&self) -> Option<(EntryPath, Byte)> {
-        if let Some(entry) = self
-            .snapshot
+    pub fn get_selected(&self) -> Option<EntrySnapshotRef<EntrySnapshot>> {
+        self.snapshot
             .as_ref()
             .and_then(|s| s.get_root().get_nth_child(self.file_list_state.selected()))
-        {
-            let mut path = self.current_path.clone();
-            path.join(entry.get_name().to_string());
-            Some((path, entry.get_size()))
-        } else {
-            None
-        }
     }
 
     pub fn go_up(&mut self) {
         if !self.current_path.is_root() {
+            if let Some(entry) = self.get_selected() {
+                // save selected entry name so if we open again this directory, it is selected again
+                self.path_history.push(entry.get_name().to_string());
+            }
             let name = self.current_path.get_name().to_string();
             self.current_path.go_up();
             self.update_snapshot();
@@ -63,23 +60,26 @@ impl FilesApp {
     }
 
     pub fn open_selected(&mut self) {
-        if let Some(snapshot) = self.snapshot.as_ref() {
-            let files: Vec<_> = snapshot.get_root().iter().collect();
-            if let Some(entry) = files.get(self.file_list_state.selected()) {
-                if entry.is_dir() {
-                    self.current_path.join(entry.get_name().to_string());
-                    self.file_list_state.select(0);
-                    self.snapshot = None;
-                    self.update_snapshot();
-                    if self
-                        .snapshot
-                        .as_ref()
-                        .map(|s| s.get_root().get_children_count())
-                        .unwrap_or(0)
-                        == 0
-                    {
-                        // dir doesn't have children, try to rescan it
-                        self.rescan(false);
+        if let Some(entry) = self.get_selected() {
+            if entry.is_dir() {
+                self.current_path.join(entry.get_name().to_string());
+                self.file_list_state.select(0);
+                self.snapshot = None;
+                self.update_snapshot();
+                if self
+                    .snapshot
+                    .as_ref()
+                    .map(|s| s.get_root().get_children_count())
+                    .unwrap_or(0)
+                    == 0
+                {
+                    // dir doesn't have children, try to rescan it
+                    self.rescan(false);
+                }
+                if let Some(name) = self.path_history.pop() {
+                    if !self.select_entry(&name) {
+                        // we opened some other dir, so clear history
+                        self.path_history.clear();
                     }
                 }
             }
@@ -96,15 +96,17 @@ impl FilesApp {
             .select(self.file_list_state.selected() + 1);
     }
 
-    pub fn select_entry(&mut self, name: &str) {
-        if let Some(snapshot) = self.snapshot.as_ref() {
-            self.file_list_state.select(
-                snapshot
-                    .get_root()
-                    .iter()
-                    .position(|e| e.get_name() == name)
-                    .unwrap_or(0),
-            );
+    pub fn select_entry(&mut self, name: &str) -> bool {
+        if let Some(pos) = self.snapshot.as_ref().and_then(|snapshot| {
+            snapshot
+                .get_root()
+                .iter()
+                .position(|e| e.get_name() == name)
+        }) {
+            self.file_list_state.select(pos);
+            true
+        } else {
+            false
         }
     }
 
@@ -266,8 +268,10 @@ impl InputHandler for App {
     fn on_key(&mut self, c: char) {
         match c {
             'd' if self.screen == Screen::Files => {
-                if let Some((path, size)) = self.files.as_mut().unwrap().get_selected() {
-                    self.dialog = Some(Box::new(DeleteDialog::new(path, size)))
+                if let Some(entry) = self.files.as_ref().unwrap().get_selected() {
+                    let mut path = self.files.as_ref().unwrap().current_path.clone();
+                    path.join(entry.get_name().to_string());
+                    self.dialog = Some(Box::new(DeleteDialog::new(path, entry.get_size())))
                 }
             }
             'f' if self.files.is_some() => self.screen = Screen::Files,
